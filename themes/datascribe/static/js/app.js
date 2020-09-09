ce1b4f9c23a4e5ad9a67b10818e88361234359bd
kalbers <ken.albers@gmail.com>
2020-09-08 22:28:26 -0400

    First commit

---
+++ themes/datascribe/static/js/app.js
@@ -0,0 +1,8 @@
+$(document).foundation();
+
+var $navList = $('.menu');
+
+$navList.on('click', 'li:not(.is-selected)', function(e){
+  $navList.find(".is-selected").removeClass("is-selected");
+  $(e.currentTarget).addClass("is-selected");
+});
